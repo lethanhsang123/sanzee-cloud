acbe2a4ad60529cffe8f5e434d1d174dbb2c12e1
sang <sangle@aggregotoricapaci.com>
2024-08-11 17:45:56 +0700

    [feat][Sentinel][Add sentinel app]

---
+++ platform/sanzee-cloud-sentinel/src/main/java/vi/legend/sanzee/sentinel/SentinelApplication.java
@@ -0,0 +1,11 @@
+package vi.legend.sanzee.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
