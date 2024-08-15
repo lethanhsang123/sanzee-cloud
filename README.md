48b426696b8874819596499f6ae7f65e8777b969
sang <lethanhsang14082001@gmail.com>
2024-08-15 20:23:40 +0700

    [feat][core][Add]

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sanzee Cloud
+
+
+## [1], Architecture
+
+![输入图片说明](./readme/architecture.jpg)
--- platform/sanzee-cloud-gateway/src/main/java/vi/legend/sanzee/gateway/utils/WebFluxUtils.java
+++ platform/sanzee-cloud-gateway/src/main/java/vi/legend/sanzee/gateway/utils/WebFluxUtils.java
@@ -1,8 +1,10 @@
 package vi.legend.sanzee.gateway.utils;
 
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.reactive.resource.ResourceUrlProvider;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,25 +23,10 @@ public class WebFluxUtils {
     }
 
 
-
-    /**
-     * 判断路径是否与路径模式匹配
-     *
-     * @param patterns 路径模式数组
-     * @param path     url
-     * @return 是否匹配
-     */
     public static boolean isPathMatch(String[] patterns, String path) {
         return isPathMatch(Arrays.asList(patterns), path);
     }
 
-    /**
-     * 判断路径是否与路径模式匹配
-     *
-     * @param patterns 路径模式字符串List
-     * @param path     url
-     * @return 是否匹配
-     */
     public static boolean isPathMatch(List<String> patterns, String path) {
         PathMatcher pathMatcher = getPathMatcher();
         for (String pattern : patterns) {
@@ -49,5 +36,17 @@ public class WebFluxUtils {
         }
         return false;
     }
+
+//    public static Mono<Void> writeJsonResponse(ServerHttpResponse response, Result<String> result) {
+//        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+//        response.setStatusCode(HttpStatus.valueOf(result.getStatus()));
+//
+//        String jsonResult = Jackson2Utils.toJson(result);
+//        byte[] bytes = jsonResult.getBytes(StandardCharsets.UTF_8);
+//
+//        DataBuffer buffer = response.bufferFactory().wrap(bytes);
+//        return response.writeWith(Flux.just(buffer));
+//    }
+
 
 }
